b4a3809cc5ee451f1a594fd0413f1e95d7820581
arloor <admin@arloor.com>
2019-06-18 10:47:17 +0800

    解决一个内存泄漏问题

--- server/src/main/java/com/arloor/socks5/server/InitHanler.java
+++ server/src/main/java/com/arloor/socks5/server/InitHanler.java
@@ -12,48 +12,49 @@ import org.slf4j.LoggerFactory;
 
 public class InitHanler extends ChannelInboundHandlerAdapter {
     private SocketChannel remoteChannel;
-    private static Logger logger= LoggerFactory.getLogger(InitHanler.class.getSimpleName());
+    private static Logger logger = LoggerFactory.getLogger(InitHanler.class.getSimpleName());
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-       if(msg instanceof Request){
-           Request request=(Request)msg;
-           if(request.getPayload()!=null&&request.getPayload().readableBytes()==4
-                   &&request.getPayload().readByte()=='i'
-                   &&request.getPayload().readByte()=='n'
-                   &&request.getPayload().readByte()=='i'
-                   &&request.getPayload().readByte()=='t'
-           ){
-               ReferenceCountUtil.release(request.getPayload());
-               Bootstrap bootstrap = new Bootstrap();
-               bootstrap.group(ServerBootstrap.workerGroup)
-                       .channel(NioSocketChannel.class)
-                       .option(ChannelOption.TCP_NODELAY, true)
-                       .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                       .handler(new ChannelInitializer<SocketChannel>() {
-                           @Override
-                           protected void initChannel(SocketChannel ch) throws Exception {
-                               remoteChannel=ch;
+        if (msg instanceof Request) {
+            Request request = (Request) msg;
+            if (request.getPayload() != null && request.getPayload().readableBytes() == 4
+                    && request.getPayload().readByte() == 'i'
+                    && request.getPayload().readByte() == 'n'
+                    && request.getPayload().readByte() == 'i'
+                    && request.getPayload().readByte() == 't'
+            ) {
+                ReferenceCountUtil.release(request.getPayload());
+                Bootstrap bootstrap = new Bootstrap();
+                bootstrap.group(ServerBootstrap.workerGroup)
+                        .channel(NioSocketChannel.class)
+                        .option(ChannelOption.TCP_NODELAY, true)
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                        .handler(new ChannelInitializer<SocketChannel>() {
+                            @Override
+                            protected void initChannel(SocketChannel ch) throws Exception {
+                                remoteChannel = ch;
 //                               ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO));
-                               ch.pipeline().addLast(new RelayHandler(ctx.channel()));
-                           }
-                       });
-               ChannelFuture future = bootstrap.connect(request.getHost(), request.getPort());
-               future.addListener(future1 -> {
-                   if (future1.isSuccess()){
-                       ctx.pipeline().remove(InitHanler.class);
-                       ctx.pipeline().addLast(new RelayPayloadHandler(remoteChannel));
+                                ch.pipeline().addLast(new RelayHandler(ctx.channel()));
+                            }
+                        });
+                ChannelFuture future = bootstrap.connect(request.getHost(), request.getPort());
+                future.addListener(future1 -> {
+                    if (future1.isSuccess()) {
+                        ctx.pipeline().remove(InitHanler.class);
+                        ctx.pipeline().addLast(new RelayPayloadHandler(remoteChannel));
 //                       System.out.println(ctx.pipeline().names());
-                       ctx.channel().writeAndFlush(Unpooled.wrappedBuffer("check".getBytes()));
-                       logger.info("Tunnel Established："+ctx.channel().remoteAddress()+"<--->"+remoteChannel.remoteAddress());
-                   }else {
-                       ctx.close();
-                   }
-               });
+                        ctx.channel().writeAndFlush(Unpooled.wrappedBuffer("check".getBytes()));
+                        logger.info("Tunnel Established：" + ctx.channel().remoteAddress() + "<--->" + remoteChannel.remoteAddress());
+                    } else {
+                        ctx.close();
+                    }
+                });
 
-           }else{
-               ReferenceCountUtil.release(request.getPayload());
-           }
-       }
+            } else {
+                if (request.getPayload() != null)
+                    ReferenceCountUtil.release(request.getPayload());
+            }
+        }
     }
 }
